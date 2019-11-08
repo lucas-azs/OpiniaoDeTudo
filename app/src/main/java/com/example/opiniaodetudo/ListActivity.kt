ff7129ac7736417d282300e9a1eb3f5efa80a4e0
Lucas Azevedo <lucasazdesigner@gmail.com>
2019-11-08 14:33:40 -0300

    Finalizado cap 6

--- app/src/main/java/com/example/opiniaodetudo/ListActivity.kt
+++ app/src/main/java/com/example/opiniaodetudo/ListActivity.kt
@@ -2,8 +2,12 @@ package com.example.opiniaodetudo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+import com.example.opiniaodetudo.Model.Review
 import com.example.opiniaodetudo.Repository.ReviewRepository
 
 
@@ -11,16 +15,36 @@ class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_review_layout)
+        setContentView(R.layout.activity_review_list_item_layout)
 
-            val listView = findViewById<ListView>(R.id.list_recyclerview)
-            val reviews = ReviewRepository.instance.listAll()
-            val stringList = reviews.map { "${it.name} - ${it.review}" }
+        val listView = findViewById<ListView>(R.id.list_recyclerview)
+        val reviews = ReviewRepository.instance.listAll()
+        //val stringList = reviews.map { "${it.name} - ${it.review}" }
 
-            val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, stringList );
-            listView.adapter = adapter
+        val adapter = object : ArrayAdapter<Review>(this, -1, reviews) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val itemView =
+                    layoutInflater.inflate(R.layout.activity_review_list_item_layout, null)
+                val item = reviews[position]
+                val textViewName = itemView.findViewById<TextView>(R.id.itemName)
+                val textViewReview = itemView.findViewById<TextView>(R.id.itemReview)
+                textViewName.text = item.name
+                textViewReview.text = item.review
+
+                return itemView
+
+            }
+        }
+
+        listView.adapter = adapter
 
 
+        this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
--- app/src/main/java/com/example/opiniaodetudo/MainActivity.kt
+++ app/src/main/java/com/example/opiniaodetudo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.opiniaodetudo
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.opiniaodetudo.Repository.ReviewRepository
 
 class MainActivity : AppCompatActivity() {
@@ -26,10 +31,28 @@ class MainActivity : AppCompatActivity() {
             ReviewRepository.instance.save(name.toString(), review.toString())
 
             startActivity(Intent(this, ListActivity::class.java))
+
+            //Fazer com que qualquer lugar da tela esconda o teclado
+            val mainContainer = findViewById<ConstraintLayout>(R.id.main_container)
+            mainContainer.setOnTouchListener { v, event ->
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+            }
         }
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.menu_list_reviews){
+            startActivity(Intent(this, ListActivity::class.java))
+            return true
+        }
+        return false
     }
 
 
